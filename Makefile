195c4bf144f97fe5e9776c274c3a9f716c304206
Martin Braquet <martin.braquet@student.uclouvain.be>
2018-08-11 09:22:16 +0200

    Add makefiles

---
+++ Makefile
@@ -0,0 +1,26 @@
+# Dossiers contenant un makefile
+SUBDIRS := $(sort $(shell find src/ -name "Makefile"))
+SUBDIRS := $(filter-out src/how_to_contribute/Makefile,$(SUBDIRS))
+SUBDIRS := $(filter-out src/q1/info-FSAB1401/exam/2009/Juin/All/src/Makefile,$(SUBDIRS))
+
+ifeq ($(MAKECMDGOALS),clean)
+	COMPILE=make clean
+else
+	COMPILE=make
+endif
+
+ifneq (,$(filter $(MAKEMCGOALS),pvc))
+	PVC:=-pvc
+endif
+
+.PHONY: all $(SUBDIRS) clean pvc
+
+all: $(SUBDIRS)
+
+$(SUBDIRS):
+	$(COMPILE) -C $(dir $@)
+
+clean: $(SUBDIRS)
+	
+pvc: $(SUBDIRS)
+	
---
+++ root.mk
@@ -0,0 +1,109 @@
+ifneq (,$(filter $(TYPE),exam test))
+  MAIN_NAME=${NAME}-${OPTION}${CODE}-${TYPE}-${YEAR}-${MONTH}-${MINMAJ}
+else
+  MAIN_NAME=${NAME}-${OPTION}${CODE}-${TYPE}
+endif
+
+ifeq ($(strip ${TYPE}),summary)
+	ifdef NUM
+		OUT_MAIN_NAME:=Synthèse${NUM}-${NAME}-${OPTION}${CODE}
+	else
+		OUT_MAIN_NAME:=Synthèse-${NAME}-${OPTION}${CODE}
+	endif
+else ifeq ($(strip ${TYPE}),notes)
+	OUT_MAIN_NAME:=CM-${NAME}-${OPTION}${CODE}
+else ifneq (,$(filter $(TYPE),exercises))
+	ifdef NUM
+		OUT_MAIN_NAME:=APE${NUM}-${NAME}-${OPTION}${CODE}
+	else
+		OUT_MAIN_NAME:=APE-${NAME}-${OPTION}${CODE}
+	endif
+	OUT_MAIN_NAME:=APE-${NAME}-${OPTION}${CODE}
+else ifeq ($(strip ${TYPE}),mcq)
+	OUT_MAIN_NAME:=QCM-${NAME}-${OPTION}${CODE}
+else ifeq ($(strip ${TYPE}),errata)
+	OUT_MAIN_NAME:=Books-${NAME}-${OPTION}${CODE}
+else ifeq ($(strip ${TYPE}),test)
+	OUT_MAIN_NAME:=${OPTION}${CODE}-${YEAR}-${MONTH}
+else ifeq ($(strip ${TYPE}),formulaire)
+	ifdef NUM
+		OUT_MAIN_NAME:=formulaire${NUM}-${NAME}-${OPTION}${CODE}
+	else
+		OUT_MAIN_NAME:=formulaire-${NAME}-${OPTION}${CODE}
+	endif
+else ifeq ($(strip ${TYPE}),exam)
+	OUT_MAIN_NAME:=${OPTION}${CODE}-${YEAR}-${MONTH}
+	ifneq (,$(filter $(strip ${MINMAJ}),Maj Majeure)) 
+		OUT_MAIN_NAME:=$(strip $(OUT_MAIN_NAME))-Majeure
+	else ifneq (,$(filter $(strip ${MINMAJ}),Min Mineure))
+		OUT_MAIN_NAME:=$(strip $(OUT_MAIN_NAME))-Mineure
+	endif
+	ifneq (,$(filter $(strip ${NAME}),meca elec))
+		OUT_MAIN_NAME:=$(strip $(OUT_MAIN_NAME))-${NAME}
+	endif
+endif
+
+INPUT:=$(strip ${QUADRI} ${NAME} ${OPTION} ${CODE} ${TYPE} ${YEAR} ${MONTH})
+
+DIRPATH:=/home/martin/Documents/OnedriveEPL/EPL-Drive
+
+PY_SCRIPT:=/home/martin/Documents/Syntheses-PDF-Builder-master/mysmartcp.py
+
+MAIN_NAME_SOL=${MAIN_NAME}-Sol
+
+ifdef SOL
+ifeq ($(SOL),only)
+  SETSOL=-s sol=-Sol
+else ifeq ($(SOL),none)
+  SETSOL=-s sol=
+endif
+endif
+
+ifneq (,$(filter $(TYPE),exam test exercises mcq))
+  ALL=$(MAIN_NAME).pdf $(MAIN_NAME_SOL).pdf
+  OUT_MAIN_NAME_SOL=${OUT_MAIN_NAME}-Sol
+else
+  ALL=${MAIN_NAME}.pdf
+endif
+
+define commit_function
+	$(eval DATE:=$(shell echo "$(shell git log -n 1 --pretty=format:"%ai" $1.tex)" | sed -r 's/([0-9]+)-([0-9]+)-([0-9]+) ([0-9]+):([0-9]+):([0-9]+) ([^ ]+)/\3\/\2\/\1\ (\4\:\5\)/'))
+	$(eval COMMIT_ID:=$(shell git log -1 --pretty=format:%h -- $1.tex))
+endef
+
+# 1e: make all
+# 2e: make only out-of-date
+# .PHONY: clean cleanaux $(ALL)
+.PHONY: clean cleanaux
+
+all: $(ALL) cleanaux
+
+$(MAIN_NAME).pdf: $(MAIN_NAME).tex
+	$(eval SUBPATH:=$(shell python3 ${PY_SCRIPT} ${INPUT}))
+	$(eval MYPATH:=${DIRPATH}/${SUBPATH})
+	$(call commit_function,$(MAIN_NAME))
+ifneq (,$(filter $(TYPE),exam test exercises mcq))
+	latexmk -pdf $(PVC) -pdflatex="pdflatex -shell-escape -enable-write18 '\def\Sol{false} \def\DATUM{${DATE}} \def\COMMITID{${COMMIT_ID}} \def\COMMITINFOS{} \input{%S}'" -use-make $(MAIN_NAME).tex
+else
+	latexmk -pdf $(PVC) -pdflatex="pdflatex -shell-escape -enable-write18 '\def\DATUM{${DATE}} \def\COMMITID{${COMMIT_ID}} \def\COMMITINFOS{} \input{%S}'" -use-make $(MAIN_NAME).tex
+endif
+	mkdir -p "${MYPATH}"
+	cp  "$(MAIN_NAME).pdf" "${MYPATH}/$(OUT_MAIN_NAME).pdf"
+
+$(MAIN_NAME_SOL).pdf: $(MAIN_NAME).tex
+	$(eval SUBPATH:=$(shell python3 ${PY_SCRIPT} ${INPUT}))
+	$(eval MYPATH:=${DIRPATH}/${SUBPATH})
+	$(call commit_function,$(MAIN_NAME))
+	latexmk -pdf $(PVC) -pdflatex="pdflatex -jobname=$(MAIN_NAME_SOL) -shell-escape -enable-write18 '\def\Sol{true} \def\DATUM{${DATE}} \def\COMMITINFOS{} \def\COMMITID{${COMMIT_ID}} \input{%S}'" \
+	  -use-make $(MAIN_NAME).tex -jobname=$(MAIN_NAME_SOL)
+	mkdir -p "${MYPATH}"
+	cp "$(MAIN_NAME_SOL).pdf" "${MYPATH}/$(OUT_MAIN_NAME_SOL).pdf"
+
+# Supprime tout
+clean: cleanaux
+	rm -rf $(ALL)
+
+# Supprime les fichiers auxilliaires
+cleanaux:
+	latexmk -c
+	rm -rf *.aux *.fdb_latexmk *.log *.out *.bbl *.run.xml *.toc
